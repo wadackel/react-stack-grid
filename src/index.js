6919dd9462176b15c88d30de588a2fe07a4172db
tsuyoshiwada <v23.b.wada@gmail.com>
2017-06-04 21:51:10 +0900

    Remove .js.flow
    Since the outputted file does not work properly...

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import StackGrid from './components/StackGrid';
 
 export * as easings from './animations/easings';
 export * as transitions from './animations/transitions/';
+
 export default StackGrid;
